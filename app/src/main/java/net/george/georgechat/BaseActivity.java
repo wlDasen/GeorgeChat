1ab09ddd28f43348b84ba1fcf81af3839c34fb0e
wlDasen <jiapeidi@sunniwell.net>
2017-12-22 11:38:27 +0800

    first commit

---
+++ app/src/main/java/net/george/georgechat/BaseActivity.java
@@ -0,0 +1,53 @@
+package net.george.georgechat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import butterknife.ButterKnife;
+
+/**
+ * @author George
+ * @date 2017/12/21
+ * @email georgejiapeidi@gmail.com
+ * @describe 基类Activity，简化各个子类的代码量，同时规范各个子类的接口
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    private static final String TAG = "jpd-BAc";
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate: ");
+        init();
+        setContentView(provideLayoutView());
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * 跳转Activity
+     * @param activity 要跳转的Activity的Class类
+     */
+    public void jumpToActivity(Class activity) {
+        Intent intent = new Intent(this, activity);
+        startActivity(intent);
+    }
+
+    /**
+     * 子类Activity提供布局ResourceID
+     * @return 布局ID
+     */
+    protected abstract int provideLayoutView();
+
+    /**
+     * 子类展示UI之前做的一些操作（比如动态申请权限等）
+     */
+    public void init() {}
+
+    /**
+     * 子类初始化监听器接口
+     */
+    public void initListener() {}
+}
---
+++ app/src/main/java/net/george/georgechat/MainActivity.java
@@ -0,0 +1,55 @@
+package net.george.georgechat;
+
+import android.Manifest;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Method;
+
+import butterknife.BindView;
+import george.net.george_permission.GeorgePermission;
+
+/**
+ * @author George
+ * @date 2017/12/21
+ * @email georgejiapeidi@gmail.com
+ * @describe 应用程序的初始Activity
+ */
+public class MainActivity extends BaseActivity {
+    private static final String TAG = "jpd-MAc";
+    @BindView(R.id.btnLogin)
+    Button loginBtn;
+    @BindView(R.id.btnRegister)
+    Button registerBtn;
+
+    @Override
+    public void init() {
+        GeorgePermission.with(MainActivity.this).addRequestCode(100).permissions(
+                Manifest.permission.READ_CALENDAR,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.READ_EXTERNAL_STORAGE).request();
+    }
+
+    @Override
+    public void initListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jumpToActivity(LoginActivity.class);
+            }
+        });
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                jumpToActivity(RegisterActivity.class);
+            }
+        });
+    }
+
+    @Override
+    protected int provideLayoutView() {
+        return R.layout.activity_main;
+    }
+}
